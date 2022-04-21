98c66f27625aa7f70651b3051d1ae44358dd5fa4
KusalinTH <kt1218@ic.ac.uk>
2022-04-21 16:45:38 +0700

    superuser autntication

--- backend/medadmin/views.py
+++ backend/medadmin/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import MedAdmin
 from .serializers import MedAdminSerializer
@@ -8,6 +8,7 @@ from .serializers import MedAdminSerializer
 class MedAdminListCreateAPIView(generics.ListCreateAPIView):
     queryset = MedAdmin.objects.all()
     serializer_class = MedAdminSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 medadmin_listcreate_view = MedAdminListCreateAPIView.as_view()
 
